fd7e770a9c2d3567a76899982d681699ecec74f2
ARTEAGA1811 <bryan.arteaga@epn.edu.ec>
2022-11-14 17:55:42 -0500

    Tipos de variables

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,32 @@
+// 01-javascript
+// 01-variables.js
+
+// mutable (re asignadas =) e inmutables.
+var a = 1;
+let b = 2;
+
+a = 3;
+b = 4;
+
+//Inmutables
+const c = 5;
+
+
+//Tipos de variables (primitivas)
+//String
+const nombre = "Juan";
+//Number
+const edad = 30;
+//Boolean
+const tieneTrabajo = true;
+//Null
+const carro = null;
+//Undefined
+let casa;
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof tieneTrabajo);
+console.log(typeof carro);
+console.log(typeof casa);
+
